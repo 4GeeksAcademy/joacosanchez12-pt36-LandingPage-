79d19af4b570c34c106857e4d988987b86a8e5e3
kinnetik0 <alexis.rosquete@gmail.com>
2024-06-06 16:17:54 +0200

    Update React Proyect -Creating Props

--- src/js/component/Card.jsx
+++ src/js/component/Card.jsx
@@ -1,22 +1,22 @@
 import React from "react";
 import colores from "../../img/colores.jpg";
 
-const Card = () => {
+const Card = (props) => {
     return (
         <div className="col mb-3">
             <div className="card h-100">
-                <img src={colores} className="card-img-top" alt="image" />
+                <img
+                    src={props.image || colores}
+                    className="card-img-top"
+                    alt="image"
+                />
                 <div className="card-body">
-                    <h5 className="card-title text-center">Card title</h5>
-                    <p className="card-text text-center">
-                        Lorem ipsum dolor, sit amet consectetur adipisicing
-                        elit. Sequi esse rerum pariatur illo, aut optio eum
-                        nulla doloremque veniam.
-                    </p>
+                    <h5 className="card-title text-center">{props.title}</h5>
+                    <p className="card-text text-center">{props.description}</p>
                 </div>
                 <div className="card-footer">
-                    <button type="button" class="btn btn-primary">
-                        Find out more
+                    <button type="button" class="buttonCard btn btn-primary">
+                        {props.button}
                     </button>
                 </div>
             </div>
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -11,10 +11,26 @@ const Home = () => {
             <div className="container">
                 <Jumbotron />
                 <div className="row row-cols-1 row-cols-md-4 mt-3">
-                    <Card />
-                    <Card />
-                    <Card />
-                    <Card />
+                    <Card
+                        title="First Card"
+                        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+                        button="Find out more!"
+                    />
+                    <Card
+                        title="Second Card"
+                        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+                        button="Find out more!"
+                    />
+                    <Card
+                        title="Third Card"
+                        description="Aenean dolor libero, ullamcorper id tempor in, laoreet id odio. Duis maximus massa at ex vehicula, eu pulvinar urna tincidunt. Nullam et sapien sed eros ultrices ornare. Sed feugiat egestas viverra. Fusce eu rhoncus nisi"
+                        button="Find out more!"
+                    />
+                    <Card
+                        title="Fourth Card"
+                        description="Cras non pharetra augue. Etiam nec viverra odio. Vestibulum molestie, mauris ut feugiat condimentum, massa turpis ornare erat, quis egestas elit nulla vel libero. Duis ac arcu sit amet elit pellentesque molestie."
+                        button="Find out more!"
+                    />
                 </div>
             </div>
             <Footer />
