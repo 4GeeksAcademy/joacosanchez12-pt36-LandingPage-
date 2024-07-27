e3ba01276707251b9a1032bd47559183d9be2575
Michflorez <167869106+Michflorez@users.noreply.github.com>
2024-07-27 12:52:52 +0000

    michflorez landing

---
+++ src/js/component/Card.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Card = ({ title, text }) => {
+  return (
+    <div className="card">
+      <img src="http://via.placeholder.com/500x325" className="card-img-top" alt="..." />
+      <div className="card-body">
+        <h5 className="card-title">{title}</h5>
+        <p className="card-text">{text}</p>
+        <a href="#" className="btn btn-primary">Find Out More!</a>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
---
+++ src/js/component/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="footer bg-dark text-white text-center py-3">
+      <div className="container">
+        <span>Copyright © Your Website 2019</span>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
